e88a5ef097e3812a677c64c846b4d8c8bebd6828
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-04-18 18:46:35 +0530

    Some practice using javascript

--- Website/index.js
+++ Website/index.js
@@ -1,11 +1,13 @@
-function function1(){
-    let x = 5; //Local
-    console.log(x);
+function openFridge(...foods){ //used rest parameter to combine all the food into one single foods
+    console.log(foods); // -> "Vada Pav", "Dosa", "Samosa", "Puran Poli", "Varan Bhat"
+    console.log(...foods); //This is spread operator to unpack the array
+    // -> Vada Pav Dosa Samosa Puran Poli Varan Bhat
 }
 
-function function2(){
-    let x = 9; //Local
-    console.log(x);
-}
+let food1 = "Vada Pav";
+let food2 = "Dosa";
+let food3 = "Samosa";
+let food4 = "Puran Poli";
+let food5 = "Varan Bhat";
 
-function1(); // -> 5
+openFridge(food1, food2, food3, food4, food5);

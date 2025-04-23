0efcf9332db201e3bbd403b0b578d48ca470076e
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-04-23 23:36:22 +0530

    Practice of syntax and logic

--- Website/index.js
+++ Website/index.js
@@ -1,7 +1,28 @@
-const prices = [5, 35, 10, 45, 8];
-const total = prices.reduce(sum);
-console.log(`$${total.toFixed(2)}`);
+class Product{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
 
-function sum(accumulator, element){
-    return accumulator + element;
+    displayProduct(){   //No need to use functio keyword inside the class
+        console.log(`Product: ${this.name}`);
+        console.log(`Price: $${this.price.toFixed(2)}`);
+    }
+
+    calculateTotal(salesTax){
+        return this.price + (this.price * salesTax);
+    }
 }
+const salesTax = 0.05;
+
+const product1 = new Product("shirt", 19.99);
+const product2 = new Product("pant", 22.5);
+// Constructor will be called automatically just pass the argument
+
+product1.displayProduct();
+const total1 = product1.calculateTotal(salesTax);
+console.log(`Total price(including tax) is: $${total1.toFixed(2)}`);
+
+product2.displayProduct();
+const total2 = product1.calculateTotal(salesTax);
+console.log(`Total price(including tax) is: $${total2.toFixed(2)}`);

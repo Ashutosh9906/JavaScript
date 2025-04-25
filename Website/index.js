d623192d3646d95bfb2aa0592ae07d658bcbbc70
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-04-25 23:36:18 +0530

    Practice

--- Website/index.js
+++ Website/index.js
@@ -1,35 +1,41 @@
-class Animal{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
     }
-}
-class Rabbit extends Animal{
-    constructor(name, age, runSpeed){
-        super(name, age);
-        this.runSpeed = runSpeed;
-    }
-}
-class Fish extends Animal{
-    constructor(name, age, swimSpeed){
-        super(name, age);
-        this.swimSpeed = swimSpeed;
-    }
-}
-class Hawk extends Animal{
-    constructor(name, age, flySpeed){
-        super(name, age);
-        this.flySpeed = flySpeed;
-    }
-}
 
-const rabbit = new Rabbit("rabbit", 10, 25);
-const fish = new Fish("fish", 25, 56);
-const hawk = new Hawk("hawk", 18, 70);
+    set width(newWidth){
+        if(newWidth > 0){
+            this._width = newWidth; //here this_width implies that this is an private propert
+        }
+        else{
+            console.error("Width must be positive number!");
+        }
+    }
 
-console.log(fish.name);
-console.log(fish.age);
-console.log(fish.swimSpeed)
+    set height(newHeight){
+        if(newHeight > 0){
+            this._height = newHeight; //here this_width implies that this is an private propert
+        }
+        else{
+            console.error("Height must be positive number!");
+        }
+    }
 
-//Before using this keyword in child class we mush call the constructor of the parent class
-//Using Super it will be more code reusability
+    get width(){
+        return this._width;
+    }
+
+    get height(){
+        return this._height;
+    }
+
+    get area(){
+        return this._width * this._height;
+    }
+}
+const rectangle = new Rectangle(3, 4);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
